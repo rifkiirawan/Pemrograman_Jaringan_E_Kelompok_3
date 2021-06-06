92dcf883a6802305b3e5c5c95b289485edaa2879
rifkiirawan <rifkiauliairawan@gmail.com>
2021-06-06 17:23:08 +0700

    Update

--- Tugas 1 Kelompok 3/client_GUI.py
+++ Tugas 1 Kelompok 3/client_GUI.py
@@ -22,12 +22,7 @@ class GUI:
         self.login.resizable(width=False, height=False)
         self.login.configure(width=400, height=350, bg="#30475E")
 
-        self.pls = tk.Label(self.login,
-                            text="SILAHKAN LOGIN",
-                            justify=tk.CENTER,
-                            font="Montserrat 15 bold",
-                            fg="#dddddd",
-                            bg="#30475E")
+        self.pls = tk.Label(self.login, text="SILAHKAN LOGIN", justify=tk.CENTER, font="Montserrat 15 bold", fg="#dddddd", bg="#30475E")
         self.pls.place(relheight=0.15, relx=0.29, rely=0.07)
 
         self.userLabelName = tk.Label(self.login, text="Username : ", font="Montserrat 12 bold", fg="#dddddd", bg="#30475E")
@@ -43,11 +38,7 @@ class GUI:
         self.roomEntryName = tk.Entry(self.login, font="Montserrat 11", show="*")
         self.roomEntryName.place(relwidth=0.4 ,relheight=0.1, relx=0.35, rely=0.45)
 
-        self.go = tk.Button(self.login,
-                            text="Lanjutkan",
-                            font="Montserrat 12 bold",
-                            fg = "#dddddd",
-                            bg = "#F05454",
+        self.go = tk.Button(self.login, text="Lanjutkan", font="Montserrat 12 bold", fg = "#dddddd", bg = "#F05454",
                             command = lambda: self.goAhead(self.userEntryName.get(), self.roomEntryName.get()))
 
         self.go.place(relx=0.35, rely=0.62)
@@ -61,22 +52,17 @@ class GUI:
         self.server.send(str.encode(room_id))
 
         self.login.destroy()
-        self.layout()
+        self.tampilan()
 
-        rcv = threading.Thread(target=self.receive)
+        rcv = threading.Thread(target=self.terima)
         rcv.start()
 
-    def layout(self):
+    def tampilan(self):
         self.Window.deiconify()
         self.Window.title("CHATROOM")
         self.Window.resizable(width=False, height=False)
         self.Window.configure(width=470, height=550, bg="#222831")
-        self.chatBoxHead = tk.Label(self.Window,
-                                    bg = "#222831",
-                                    fg = "#dddddd",
-                                    text = self.name ,
-                                    font = "Montserrat 11 bold",
-                                    pady = 5)
+        self.chatBoxHead = tk.Label(self.Window, bg = "#222831", fg = "#dddddd", text = self.name, font = "Montserrat 11 bold", pady = 5)
 
         self.chatBoxHead.place(relwidth = 1)
 
@@ -84,14 +70,7 @@ class GUI:
 
         self.line.place(relwidth = 1, rely = 0.07, relheight = 0.012)
 
-        self.textCons = tk.Text(self.Window,
-                                width=20,
-                                height=2,
-                                bg="#30475E",
-                                fg="#dddddd",
-                                font="Montserrat 11",
-                                padx=5,
-                                pady=5)
+        self.textCons = tk.Text(self.Window, width=20, height=2, bg="#30475E", fg="#dddddd", font="Montserrat 11", padx=5, pady=5)
 
         self.textCons.place(relheight=0.745, relwidth=1, rely=0.08)
 
@@ -99,22 +78,12 @@ class GUI:
 
         self.labelBottom.place(relwidth = 1, rely = 0.8)
 
-        self.entryMsg = tk.Entry(self.labelBottom,
-                                bg = "#30475E",
-                                fg = "#dddddd",
-                                font = "Montserrat 11")
-        self.entryMsg.place(relwidth = 0.65,
-							relheight = 0.03,
-							rely = 0.008,
-							relx = 0.011)
+        self.entryMsg = tk.Entry(self.labelBottom, bg = "#30475E", fg = "#dddddd", font = "Montserrat 11")
+        self.entryMsg.place(relwidth = 0.65, relheight = 0.03, rely = 0.008, relx = 0.011)
         self.entryMsg.focus()
 
-        self.buttonMsg = tk.Button(self.labelBottom,
-								text = "Kirim",
-								font = "Montserrat 10 bold",
-								width = 20,
-								bg = "#F05454",
-								command = lambda : self.sendButton(self.entryMsg.get()))
+        self.buttonMsg = tk.Button(self.labelBottom, text = "Kirim", font = "Montserrat 10 bold", width = 20, bg = "#F05454",
+								command = lambda : self.tombolSend(self.entryMsg.get()))
         self.buttonMsg.place(relx = 0.67,
 							rely = 0.008,
 							relheight = 0.03,
@@ -125,38 +94,15 @@ class GUI:
         self.labelFile.place(relwidth = 1,
 							    rely = 0.9)
 
-        self.fileLocation = tk.Label(self.labelFile,
-                                text = "Pilih File yang Akan Dikirim",
-                                bg = "#30475E",
-                                fg = "#EAECEE",
-                                font = "Montserrat 11")
-        self.fileLocation.place(relwidth = 0.65,
-                                relheight = 0.03,
-                                rely = 0.008,
-                                relx = 0.011)
+        self.fileLocation = tk.Label(self.labelFile, text = "Pilih File yang Akan Dikirim", bg = "#30475E", fg = "#EAECEE", font = "Montserrat 11")
+        self.fileLocation.place(relwidth = 0.65, relheight = 0.03, rely = 0.008, relx = 0.011)
 
-        self.browse = tk.Button(self.labelFile,
-								text = "Browse",
-								font = "Montserrat 10 bold",
-								width = 13,
-								bg = "#F05454",
-								command = self.browseFile)
+        self.browse = tk.Button(self.labelFile, text = "Browse", font = "Montserrat 10 bold", width = 13, bg = "#F05454", command = self.browse)
 
-        self.browse.place(relx = 0.67,
-							rely = 0.008,
-							relheight = 0.03,
-							relwidth = 0.15)
+        self.browse.place(relx = 0.67, rely = 0.008, relheight = 0.03, relwidth = 0.15)
 
-        self.sengFileBtn = tk.Button(self.labelFile,
-								text = "Kirim File",
-								font = "Montserrat 10 bold",
-								width = 13,
-								bg = "#F05454",
-								command = self.sendFile)
-        self.sengFileBtn.place(relx = 0.84,
-							rely = 0.008,
-							relheight = 0.03,
-							relwidth = 0.15)
+        self.sengFileBtn = tk.Button(self.labelFile, text = "Kirim File", font = "Montserrat 10 bold", width = 13, bg = "#F05454", command = self.kirimFile)
+        self.sengFileBtn.place(relx = 0.84, rely = 0.008, relheight = 0.03, relwidth = 0.15)
 
         self.textCons.config(cursor = "arrow")
         scrollbar = tk.Scrollbar(self.textCons)
@@ -166,7 +112,7 @@ class GUI:
         scrollbar.config(command = self.textCons.yview)
         self.textCons.config(state = tk.DISABLED)
 
-    def browseFile(self):
+    def browse(self):
         self.filename = filedialog.askopenfilename(initialdir="/",
                                     title="Silahkan Pilih File",
                                     filetypes = (("Text files",
@@ -175,7 +121,7 @@ class GUI:
                                                 "*.*")))
         self.fileLocation.configure(text="File yang Akan Dikirim: "+ self.filename)
 
-    def sendFile(self):
+    def kirimFile(self):
         self.server.send("FILE".encode())
         time.sleep(0.1)
         self.server.send(str("~~Menerima~~ " + os.path.basename(self.filename)).encode())
@@ -196,14 +142,14 @@ class GUI:
         self.textCons.config(state = tk.DISABLED)
         self.textCons.see(tk.END)
 
-    def sendButton(self, msg):
+    def tombolSend(self, msg):
         self.textCons.config(state = tk.DISABLED)
         self.msg=msg
         self.entryMsg.delete(0, tk.END)
-        snd= threading.Thread(target = self.sendMessage)
+        snd= threading.Thread(target = self.kirimPesan)
         snd.start()
 
-    def receive(self):
+    def terima(self):
         while True:
             try:
                 message = self.server.recv(1024).decode()
@@ -243,7 +189,7 @@ class GUI:
                 self.server.close()
                 break
 
-    def sendMessage(self):
+    def kirimPesan(self):
         self.textCons.config(state=tk.DISABLED)
         while True:
             self.server.send(self.msg.encode())
