ea66bcc3e147381fa03d013894b98aa98367b600
rifkiirawan <rifkiauliairawan@gmail.com>
2021-07-17 12:54:00 +0700

    fix no. 2

--- FinalProject/2/server_thread_http.py
+++ FinalProject/2/server_thread_http.py
@@ -1,12 +1,12 @@
 from socket import *
 import socket
 import threading
+import time
 import sys
 import logging
-from http import HttpServer
+from lb import LoadBalancer
 
-httpserver = HttpServer()
-
+reverseProxy = LoadBalancer()
 
 class ProcessTheClient(threading.Thread):
 	def __init__(self, connection, address):
@@ -18,25 +18,18 @@ class ProcessTheClient(threading.Thread):
 		rcv=""
 		while True:
 			try:
-				data = self.connection.recv(32)
+				data = self.connection.recv(8192)
+				self.destination_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 				if data:
-					#merubah input dari socket (berupa bytes) ke dalam string
-					#agar bisa mendeteksi \r\n
-					d = data.decode()
-					rcv=rcv+d
-					if rcv[-2:]=='\r\n':
-						#end of command, proses string
-						logging.warning("Data Dari Client => {}" . format(rcv))
-						hasil = httpserver.proses(rcv)
-						#hasil akan berupa bytes
-						#untuk bisa ditambahi dengan string, maka string harus di encode
-						hasil=hasil+"\r\n\r\n".encode()
-						logging.warning("Balas Ke Client => {}" . format(hasil))
-						#hasil sudah dalam bentuk bytes
-						self.connection.sendall(hasil)
-						rcv=""
-						self.connection.close()
-						break
+					server = reverseProxy.get_server()
+					print(f"forwarded to server {server}")
+					self.destination_sock.connect(server)
+					self.destination_sock.sendall(data)
+					data_balasan = self.destination_sock.recv(8192)
+					self.connection.sendall(data_balasan)
+					logging.warning(data)
+					logging.warning(data_balasan)
+					break
 				else:
 					break
 			except OSError as e:
@@ -46,32 +39,26 @@ class ProcessTheClient(threading.Thread):
 
 
 class Server(threading.Thread):
-	def __init__(self, port=9000):
+	def __init__(self):
 		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.port = port
 		threading.Thread.__init__(self)
 
 	def run(self):
-		self.my_socket.bind(('0.0.0.0', self.port))
-		self.my_socket.listen(1)
+		self.my_socket.bind(('0.0.0.0', 18000))
+		self.my_socket.listen(5)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
-			logging.warning("Koneksi Dari {}".format(self.client_address))
+			logging.warning("connection from {}".format(self.client_address))
 
 			clt = ProcessTheClient(self.connection, self.client_address)
-			clt.start()	
+			clt.start()
 
 
 
 def main():
-	if len(sys.argv) > 1 :
-		svr = Server(int(sys.argv[1]))
-		svr.start()
-	else:
-		svr = Server()
-		svr.start()
+	svr = Server()
+	svr.start()
 
 if __name__=="__main__":
 	main()
-
